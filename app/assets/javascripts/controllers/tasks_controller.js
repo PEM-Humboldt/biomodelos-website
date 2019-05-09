bca4ac97cbfec41ad48574bd7d18c8af409291aa
Erika Suárez Valencia <marvinesv@gmail.com>
2019-05-09 14:06:30 -0500

    Change all javascript controllers from crlf to lf

--- app/assets/javascripts/controllers/tasks_controller.js
+++ app/assets/javascripts/controllers/tasks_controller.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	/**
- 	* Asks for a confirmation of the finish task action. If it's positive, 
+ 	* Asks for a confirmation of the finish task action. If it's positive,
  	* sends an ajax request to finish the task.
 	* @param {string} task_id - ID of the task to be marked as finished.
 	* @param {string} origin - origin of the action: group or user.
@@ -17,8 +17,8 @@ $(document).ready(function(){
 		    		},
 					error: function(jqXHR, textStatus, errorThrown){
 						alertify.alert("Ha ocurrido un error al actualizar la tarea: " + textStatus);
-					}	
-				});	
+					}
+				});
 			}
 			else{
 				$("#task_active_"+task_id).prop('checked', false);
@@ -28,13 +28,13 @@ $(document).ready(function(){
 
 	/**
  	* Gets the ID of the task being checked and calls a function to mark the task as finished.
-	* 
+	*
 	*/
 	$("body").on("change", "#group_tasks_table input[type=checkbox]", function () {
 		var task_id = $(this).val();
 		_markFinishedTask(task_id, "group");
   	});
-  	
+
   	$("body").on("change", "#user_tasks_table input[type=checkbox]", function () {
 		var task_id = $(this).val();
 		_markFinishedTask(task_id, "user");
