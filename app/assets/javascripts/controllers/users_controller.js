6cca2bc0aef1d8210993ba2baaf451c750726675
Erika Suárez Valencia <esuarez@humboldt.org.co>
2020-10-02 08:45:36 -0500

    Remove niceScroll from user page

--- app/assets/javascripts/controllers/users_controller.js
+++ app/assets/javascripts/controllers/users_controller.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-    $('.cajaperfil').niceScroll({
-    	cursorcolor: "#5d5e5e",
-    	cursorwidth: "7px",
-    	cursorborder: "none"
-	});
-
-	$('#centerbox').niceScroll({
-		cursorcolor: "#5d5e5e",
-		cursorwidth: "7px",
-		cursorborder: "none"
-	});
-
 	$( ".tareaspp:odd" ).css( "background-color", "#f2f2f2" );
 
 	$(".user_tasks_tab").click(function(e){
