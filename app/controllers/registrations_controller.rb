740016b4f69544ea28b2e426a204f9d27dc5f5ad
Manuel Gálvez <mgalvez@humboldt.org.co>
2024-02-20 17:58:18 -0500

    Update recaptcha to version 3 for registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
     def check_captcha
-      unless verify_recaptcha
+      success = verify_recaptcha(action: 'user_registration', minimum_score: 0.8, secret_key: Rails.application.secrets.reCaptcha_secret)
+      checkbox_success = verify_recaptcha unless success  
+      if success || checkbox_success
         self.resource = resource_class.new sign_up_params
         respond_with_navigational(resource) { render :new }
       end 
