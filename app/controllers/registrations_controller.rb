390b5d7a0ad3e2b2c1bef86f0cdd78259049bb7a
Camilo Zapata <cazm013@gmail.com>
2024-08-02 11:56:20 -0500

    Add logic to handle both reCAPTCHA v2 and v3 simultaneously in the signup form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,15 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
     def check_captcha
-      recaptcha_valid = verify_recaptcha(action: 'registration', minimum_score: 0.8)
-      if recaptcha_valid
+      recaptcha_v3_valid = verify_recaptcha(action: 'registration', minimum_score: 0.8, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+		  recaptcha_v2_valid = verify_recaptcha unless recaptcha_v3_valid
+      if !(recaptcha_v3_valid || recaptcha_v2_valid)
+        if !recaptcha_v3_valid
+          @show_checkbox_recaptcha = true
+        end
         self.resource = resource_class.new sign_up_params
         respond_with_navigational(resource) { render :new }
-      end 
+      end
     end
     
   protected
