0711ca1b09630d5ef9bf10d62f193632bdd44056
Camilo Zapata <cazm013@gmail.com>
2024-08-12 09:33:24 +0200

    Fix indentation tabs and spaces

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: [:create]
+  	prepend_before_action :check_captcha, only: [:create]
 
-  private
-    def check_captcha
-      recaptcha_v3_valid = verify_recaptcha(action: 'registration', minimum_score: 0.8, secret_key: Rails.application.secrets.reCaptcha_secret_v3)
-	    recaptcha_v2_valid = verify_recaptcha unless recaptcha_v3_valid
-      if !(recaptcha_v3_valid || recaptcha_v2_valid)
-        if !recaptcha_v3_valid
-          @show_checkbox_recaptcha = true
-        end
-        self.resource = resource_class.new sign_up_params
-        respond_with_navigational(resource) { render :new }
-      end
-    end
+	private
+		def check_captcha
+			recaptcha_v3_valid = verify_recaptcha(action: 'registration', minimum_score: 0.8, secret_key: Rails.application.secrets.reCaptcha_secret_v3)
+			recaptcha_v2_valid = verify_recaptcha unless recaptcha_v3_valid
+			if !(recaptcha_v3_valid || recaptcha_v2_valid)
+				if !recaptcha_v3_valid
+					@show_checkbox_recaptcha = true
+				end
+				self.resource = resource_class.new sign_up_params
+				respond_with_navigational(resource) { render :new }
+			end
+		end
     
-  protected
-    def after_update_path_for(resource)
-    	current_user
-	  end
+	protected
+		def after_update_path_for(resource)
+			current_user
+		end
 end
