d39ad866554c2c69845f49d1439a244fdd840285
jlhiguitac <jluis.higuita@gmail.com>
2025-02-28 10:06:22 -0500

    Indentation fix

--- app/mailers/administrators_mailer.rb
+++ app/mailers/administrators_mailer.rb
@@ -2,16 +2,16 @@ class AdministratorsMailer < ApplicationMailer
 	
 	def contact_us(message)
 		@message = message
-	  @datetime = DateTime.now
-	  mail to: "biomodelos@humboldt.org.co",  subject: "BioModelos: Formulario de contacto"
+		@datetime = DateTime.now
+		mail to: "biomodelos@humboldt.org.co",  subject: "BioModelos: Formulario de contacto"
 	end
 		
-  def model_uploaded(user, publication)
+	def model_uploaded(user, publication)
 		@user = user
-	    @publication = publication
-	    @datetime = DateTime.now
-	    mail to: "biomodelos@humboldt.org.co",  subject: "BioModelos: Nuevo modelo publicado por experto"
-  end
+		@publication = publication
+		@datetime = DateTime.now
+		mail to: "biomodelos@humboldt.org.co",  subject: "BioModelos: Nuevo modelo publicado por experto"
+	end
 	    
 	def group_suggested(user, new_group)
 		@user = user
