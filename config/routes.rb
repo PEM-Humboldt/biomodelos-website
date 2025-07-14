d498a46669573c6d810e673851bd3689665a77ba
jlhiguitac <jluis.higuita@gmail.com>
2025-07-14 17:01:55 -0500

    Added some configurations to unlock account

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,7 @@ Rails.application.routes.draw do
     # Example of named route that can be invoked with purchase_url(id: product.id)
     #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+ 
     # Example resource route (maps HTTP verbs to controller actions automatically):
     #   resources :products
     resources :users, :only => [:show]
