4c82dc4c5006b4d65d417f00fabeb69c8ce449bc
Jorge Higuita <jluis.higuita@gmail.com>
2026-02-17 10:13:01 -0500

    Add more instructions for use develop environment with docker.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # Raise error when a before_action's only/except options reference missing actions.
-  config.action_controller.raise_on_missing_callback_actions = true
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+  
+  # config.web_console.allowed_ips = 'x.x.x.x/xx'
 end
