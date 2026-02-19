dd7fa10ee5863e5968e8a731be80ef512a32975d
Jorge Higuita <jluis.higuita@gmail.com>
2026-02-19 13:31:40 -0500

    Added i18n and bower components

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module BioModelosV2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
+
+    config.encoding = "utf-8"
+
+    # Adding bower_components to asset pipeline
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
   end
 end
