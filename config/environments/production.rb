03d25d5f57de755d21e4a0421fd297f207f5fa4d
Jorge Higuita <jluis.higuita@gmail.com>
2026-02-26 10:12:49 -0500

    Config assets digest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  #config.assets.digest = true
+  config.assets.digest = true
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
