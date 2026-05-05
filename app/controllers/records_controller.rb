e40778007bb0549a6bab8c10879afdada788be51
Jorge Higuita <jluis.higuita@gmail.com>
2026-05-05 16:58:51 -0500

    Revert "Added date for check at API"
    This reverts commit 55fd3f9bcc4efee15e260ce4509deb692cd99dd9.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -42,6 +42,7 @@ class RecordsController < ApplicationController
         new_record[:year] = mydate.year
         new_record[:month] = mydate.month
         new_record[:day] = mydate.day
+        new_record.delete(:date)
       end
       Record.new_record(new_record)
       @alerts_to_show.push({
